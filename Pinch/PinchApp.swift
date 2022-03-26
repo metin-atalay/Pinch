00c44db5d3d02539ab4d9a898fe9d1316b062f21
Metin ATALAY <metin.atalay@architecht.com>
2022-03-26 17:46:02 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Metin Atalay on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
